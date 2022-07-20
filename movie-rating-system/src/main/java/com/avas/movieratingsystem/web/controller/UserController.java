178f0a88be24cd58b34f8a4385698fc81cc62b40
maksgris <maksgris@gmail.com>
2022-07-20 15:40:21 +0300

    Controller now returns ResponseEntity

--- movie-rating-system/src/main/java/com/avas/movieratingsystem/web/controller/UserController.java
+++ movie-rating-system/src/main/java/com/avas/movieratingsystem/web/controller/UserController.java
@@ -3,6 +3,7 @@ package com.avas.movieratingsystem.web.controller;
 import com.avas.movieratingsystem.business.service.UserService;
 import com.avas.movieratingsystem.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,11 @@ public class UserController {
     UserService userService;
 
     @GetMapping
-    public List<User> getAllUsers(){
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> userList= userService.getAllUsers();
+        if(userList.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userList);
     }
 }
