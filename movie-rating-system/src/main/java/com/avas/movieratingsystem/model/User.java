178c5ace3999eb7c81da2f56b1bba609688fe171
maksgris <maksgris@gmail.com>
2022-07-20 15:48:30 +0300

    Controller, getters and setters for the User model

--- movie-rating-system/src/main/java/com/avas/movieratingsystem/model/User.java
+++ movie-rating-system/src/main/java/com/avas/movieratingsystem/model/User.java
@@ -9,9 +9,36 @@ import javax.persistence.GenerationType;
 @Entity
 @Table(name="user")
 public class User {
+    public User() {
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
