e53beaeb2ba0fbf4460c708e95d7850a010476d3
maksgris <maksgris@gmail.com>
2022-07-28 14:35:08 +0300

    Refactored index endpoint to main controller

---
+++ movie-rating-system/src/main/java/com/avas/movieratingsystem/web/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.avas.movieratingsystem.web.controller;
+
+import com.avas.movieratingsystem.business.service.UserService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+@Log4j2
+public class MainController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/index")
+    public String showUserList(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        log.info("fetched all users");
+        return "index";
+    }
+}
--- movie-rating-system/src/main/java/com/avas/movieratingsystem/web/controller/UserController.java
+++ movie-rating-system/src/main/java/com/avas/movieratingsystem/web/controller/UserController.java
@@ -2,6 +2,7 @@ package com.avas.movieratingsystem.web.controller;
 
 import com.avas.movieratingsystem.business.service.UserService;
 import com.avas.movieratingsystem.model.UserDTO;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,18 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@Log4j2
 @Controller
 @RequestMapping("api/v1/user")
 public class UserController {
 
     @Autowired
     UserService userService;
-
-    @GetMapping("/index")
-    public String showUserList(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
-        return "index";
-    }
 
     @GetMapping
     public ResponseEntity<List<UserDTO>> getAllUsers(){
