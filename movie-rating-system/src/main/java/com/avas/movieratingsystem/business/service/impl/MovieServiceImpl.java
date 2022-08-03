2456676441f7a48a8fb44577481bf85d2db6670d
maksgris <maksgris@gmail.com>
2022-08-03 17:30:50 +0300

    Implemented Movie Service

---
+++ movie-rating-system/src/main/java/com/avas/movieratingsystem/business/service/impl/MovieServiceImpl.java
@@ -0,0 +1,73 @@
+package com.avas.movieratingsystem.business.service.impl;
+
+import com.avas.movieratingsystem.business.exceptions.MovieNotFoundException;
+import com.avas.movieratingsystem.business.exceptions.ResourceAlreadyExists;
+import com.avas.movieratingsystem.business.mappers.MovieMapping;
+import com.avas.movieratingsystem.business.repository.MovieRepository;
+import com.avas.movieratingsystem.business.repository.model.Movie;
+import com.avas.movieratingsystem.business.service.MovieService;
+import com.avas.movieratingsystem.model.MovieDTO;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Log4j2
+@Service
+public class MovieServiceImpl implements MovieService {
+    @Autowired
+    MovieRepository movieRepository;
+
+    @Autowired
+    MovieMapping movieMapper;
+    public List<MovieDTO> getAllMovies() {
+        return movieRepository.findAll().stream().map(movieMapper::mapMovieToMovieDto).collect(Collectors.toList());
+
+    }
+
+    public Optional<MovieDTO> findMovieById(Long id) {
+        Optional<MovieDTO> foundMovieDto = movieRepository.findById(id)
+                .map(foundUser -> movieMapper.mapMovieToMovieDto(foundUser));
+        if (!foundMovieDto.isPresent()) {
+            log.warn("Movie with id:{} Not found", id);
+            throw new MovieNotFoundException("Movie with id:" + id + " does not exist");
+        }
+        log.info("Found movie :{}", foundMovieDto);
+        return foundMovieDto;
+    }
+
+    public void deleteMovieById(Long id) {
+        movieRepository.deleteById(id);
+        log.info("Movie with id: {} is deleted", id);
+    }
+
+    public MovieDTO createMovie(MovieDTO newMovie) {
+        boolean userAlreadyExists = movieRepository.existsByTitle(newMovie.getMovieType());
+        if(userAlreadyExists){
+            log.warn("Can not create movie, movie with this title already exists");
+            throw new ResourceAlreadyExists("Can not create movie, movie with this title already exists");
+        }
+        Movie savedMovie = movieRepository.save(movieMapper.mapMovieDtoToMovie(newMovie));
+        log.info("Movie is created : {}", savedMovie);
+        return movieMapper.mapMovieToMovieDto(savedMovie);
+    }
+
+    public MovieDTO updateMovieById(MovieDTO modifyExistingMovie, Long id) {
+        boolean userAlreadyExists = movieRepository.existsByTitle(modifyExistingMovie.getMovieType());
+        if(userAlreadyExists){
+            log.warn("Can not update movie. This movie title is already taken :{}", modifyExistingMovie.getMovieType());
+            throw new ResourceAlreadyExists("Can not update movie. This movie title is already taken");
+        }
+        modifyExistingMovie.setId(id);
+        Movie modifiedMovie = movieRepository.save(movieMapper.mapMovieDtoToMovie(modifyExistingMovie));
+        log.info("Movie is updated movie id :{}, movie is now :{}", modifiedMovie.getId(), modifiedMovie);
+        return movieMapper.mapMovieToMovieDto(modifiedMovie);
+    }
+
+    public boolean checkIfMovieExistsById(Long id) {
+        return movieRepository.existsById(id);
+    }
+}
