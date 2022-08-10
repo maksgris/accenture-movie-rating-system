85fd7d84a6f202b3c1a26f5fb2b3300fb2493aa8
maksgris <maksgris@gmail.com>
2022-08-10 16:57:14 +0300

    UserLike DTO fix

--- movie-rating-system/src/main/java/com/avas/movieratingsystem/model/UserLikeDTO.java
+++ movie-rating-system/src/main/java/com/avas/movieratingsystem/model/UserLikeDTO.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserLikeDTO {
 
     private Long id;
-    private List<Long> userIds;
-    private List<Long> reviewIds;
+    private Long userId;
+    private Long reviewId;
 }
