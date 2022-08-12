d448c96ce5e9ab49ac0e4493c258b556a9a3bfc1
maksgris <maksgris@gmail.com>
2022-08-12 16:58:24 +0300

    MovieServiceImplTest testGetAllMoviesEmpty

--- movie-rating-system/src/test/java/com/avas/movieratingsystem/business/service/impl/MovieServiceImplTest.java
+++ movie-rating-system/src/test/java/com/avas/movieratingsystem/business/service/impl/MovieServiceImplTest.java
@@ -1,9 +1,9 @@
 package com.avas.movieratingsystem.business.service.impl;
+
 import com.avas.movieratingsystem.business.mappers.MovieMapping;
 import com.avas.movieratingsystem.business.repository.MovieRepository;
 import com.avas.movieratingsystem.business.repository.model.Movie;
 import com.avas.movieratingsystem.model.MovieDTO;
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,15 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static com.avas.movieratingsystem.test.data.MovieTestData.createMovieDTO;
 import static com.avas.movieratingsystem.test.data.MovieTestData.createMovieDtoList;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -42,8 +37,8 @@ public class MovieServiceImplTest {
     private MovieServiceImpl movieService;
 
     @Test
-    @DisplayName("Testing retrieval of all interns")
-    public void testGetAllInternsSuccessfully() {
+    @DisplayName("Testing retrieval of all Movies")
+    public void testMoviesSuccessfully() {
         List<MovieDTO> movieDTOList =createMovieDtoList();
         List<Movie> movieList = movieMapping.mapMovieDtoListToMovieList(movieDTOList);
         when(movieRepository.findAll()).thenReturn(movieList);
@@ -52,4 +47,18 @@ public class MovieServiceImplTest {
         Assertions.assertTrue(movieDTOList.equals(movieDTOListReturned));
         verify(movieRepository, times(1)).findAll();
     }
+
+    @Test
+    @DisplayName("Testing retrieval of empty Movie list")
+    public void testGetAllMoviesEmpty(){
+        List<MovieDTO> emptyDtoList = new ArrayList<MovieDTO>();
+        List<Movie> emptyMovieList = movieMapping.mapMovieDtoListToMovieList(emptyDtoList);
+        when(movieRepository.findAll()).thenReturn(emptyMovieList);
+        when(mockMovieMapping.mapMovieListToMovieListDto(emptyMovieList)).thenReturn(emptyDtoList);
+        List<MovieDTO> movieDTOListReturned = movieService.getAllMovies();
+        Assertions.assertEquals(movieDTOListReturned.size(),0);
+        verify(movieRepository, times(1)).findAll();
+    }
+
+
 }
